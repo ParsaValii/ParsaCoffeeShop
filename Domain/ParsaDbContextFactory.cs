f951dc596d65d3b4968902ad968ee55007ecfa2d
ParsaValii <parsa.valizadee@gmail.com>
2023-12-13 14:20:15 +0330

    ContextInDesignTimeAdded

---
+++ Domain/ParsaDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ParsaDbContextFactory : IDesignTimeDbContextFactory<ParsaDbContext>
+    {
+        public ParsaDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ParsaDbContext>();
+            optionsBuilder.UseLazyLoadingProxies().UseNpgsql("Server=localhost;Database=ParsaCoffeeShopDb;Port=5432;User Id=postgres;Password=postgres");
+
+            return new ParsaDbContext(optionsBuilder.Options);
+        }
+    }
+}
