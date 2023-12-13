3cd47e76851551358d553da3419deb5ba4558d80
ParsaValii <parsa.valizadee@gmail.com>
2023-12-13 14:20:36 +0330

    MenuControllerAdded

---
+++ ParsaCoffeeShop/Areas/Admin/Controllers/MenusController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+using Domain.Entities;
+
+namespace ParsaCoffeeShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenusController : Controller
+    {
+        private readonly ParsaDbContext _context;
+
+        public MenusController(ParsaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Menus
+        public async Task<IActionResult> Index()
+        {
+              return _context.Menus != null ? 
+                          View(await _context.Menus.ToListAsync()) :
+                          Problem("Entity set 'ParsaDbContext.Menus'  is null.");
+        }
+
+        // GET: Admin/Menus/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Menus == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // GET: Admin/Menus/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Menus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                menu.Id = Guid.NewGuid();
+                _context.Add(menu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        // GET: Admin/Menus/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Menus == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus.FindAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Admin/Menus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Title")] Menu menu)
+        {
+            if (id != menu.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(menu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuExists(menu.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        // GET: Admin/Menus/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Menus == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // POST: Admin/Menus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Menus == null)
+            {
+                return Problem("Entity set 'ParsaDbContext.Menus'  is null.");
+            }
+            var menu = await _context.Menus.FindAsync(id);
+            if (menu != null)
+            {
+                _context.Menus.Remove(menu);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MenuExists(Guid id)
+        {
+          return (_context.Menus?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
