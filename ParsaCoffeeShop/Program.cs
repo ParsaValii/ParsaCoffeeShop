5906e246c3509af45113134ff5a8d9a99977c6d8
ParsaValii <parsa.valizadee@gmail.com>
2023-12-13 16:04:01 +0330

    MenuServiceAdded

--- ParsaCoffeeShop/Program.cs
+++ ParsaCoffeeShop/Program.cs
@@ -1,5 +1,7 @@
 using Domain;
 using Microsoft.EntityFrameworkCore;
+using Service.Interfaces;
+using Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ParsaDbContext>(options =>
     options.UseLazyLoadingProxies().UseNpgsql(builder.Configuration.GetConnectionString("ParsaDbContext")));
-
+builder.Services.AddScoped<IMenuService, MenuService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
