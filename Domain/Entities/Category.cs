6fcbc4ad8469dd5139ec5efcd56bcb1497400229
ParsaValii <parsa.valizadee@gmail.com>
2023-12-13 12:06:41 +0330

    CategoryAdded

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+
+        [Display(Name = "Title")]
+        [Required(ErrorMessage = "required")]
+        [MaxLength(150)]
+        public string Title { get; set; }
+
+
+        [Display(Name = "Image")]
+        public string Image { get; set; }
+
+
+        public virtual Menu Menu { get; set; }
+
+
+        public virtual IList<Item>? Items { get; set; }
+        public Category()
+        {
+            Items = new List<Item>();
+        }
+    }
+}
